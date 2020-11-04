9d84fc347a1ffb464b95f5aeca21bcb412f94996
Wang Da - Mech <wangda.mme@gmail.com>
2020-11-03 21:44:10 -0500

    Add: Implementate Home Page with CardStackView & Swipe for Like/Unlike

--- app/src/main/java/com/wangdamme/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/wangdamme/myapplication/ui/home/HomeFragment.java
@@ -14,15 +14,31 @@ import android.view.ViewGroup;
 
 import com.wangdamme.myapplication.R;
 import com.wangdamme.myapplication.databinding.FragmentHomeBinding;
+import com.wangdamme.myapplication.model.Article;
 import com.wangdamme.myapplication.repository.NewsRepository;
 import com.wangdamme.myapplication.repository.NewsViewModelFactory;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.Duration;
+import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeAnimationSetting;
 
+import java.util.List;
 
-public class HomeFragment extends Fragment {
+// To know a card has been swiped either by gesture or button click?
+// We need some sort of callback for it.
+// This callback is CardStackListener.
+// Now we let the HomeFragment implement the CartStackListener interface:
+public class HomeFragment extends Fragment implements CardStackListener {
 
     //Use ViewModel in the Fragments
     private HomeViewModel viewModel;
     private FragmentHomeBinding binding;  // generate by //res/layout/fragment_home
+
+    private CardStackLayoutManager layoutManager;
+
+    private List<Article> articles;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,15 +46,29 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         // return inflater.inflate(R.layout.fragment_home, container, false);
 
+        // First draw a Home Fragment View
         binding = FragmentHomeBinding.inflate(inflater,container,false);
         return binding.getRoot();
-
     }
 
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        //Setup CardStack View
+        CardSwipeAdapter swipeAdapter = new CardSwipeAdapter();
+        layoutManager = new CardStackLayoutManager(requireContext(),this);
+        // setup stack display
+        layoutManager.setStackFrom(StackFrom.Top);
+        //CardStack View actually is RecyclerView
+        binding.homeCardStackView.setLayoutManager(layoutManager);
+        binding.homeCardStackView.setAdapter(swipeAdapter);
+
+        // Handle like unlike button clicks
+        binding.homeLikeButton.setOnClickListener(v-> swipeCard(Direction.Right));
+        binding.homeUnlikeButton.setOnClickListener(v-> swipeCard(Direction.Left));
+
 
         NewsRepository repository= new NewsRepository(getContext()); // input Context
         viewModel= new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(HomeViewModel.class);
@@ -50,10 +80,67 @@ public class HomeFragment extends Fragment {
                 getViewLifecycleOwner(), newsResponse -> {
                     if(newsResponse!=null)
                     {
-                        Log.d("HomeFragment",newsResponse.toString()); // print in logCat
+                        // Log.d("HomeFragment",newsResponse.toString()); // print in logCat
+                        articles= newsResponse.articles;
+                        swipeAdapter.setArticles(articles);
                     }
                 }
         );
 
+    }
+
+    //=========== Override Listener ========//
+
+    @Override
+    public void onCardDragging(Direction direction, float ratio) {
+
+    }
+
+    @Override
+    public void onCardSwiped(Direction direction) {
+        if(direction==Direction.Left)
+        {
+            Log.d("CardStackView", "Unliked "+layoutManager.getTopPosition());
+        }
+        else if (direction==Direction.Right)
+        {
+            Log.d("CardStackView", "Liked "+layoutManager.getTopPosition());
+
+        }
+    }
+
+    @Override
+    public void onCardRewound() {
+
+    }
+
+    @Override
+    public void onCardCanceled() {
+
+    }
+
+    @Override
+    public void onCardAppeared(View view, int position) {
+
+    }
+
+    @Override
+    public void onCardDisappeared(View view, int position) {
+
+    }
+    //=========== Override Listener ========//
+
+
+
+    // Implement Swipe Events
+    private void swipeCard(Direction direction)
+    {
+        SwipeAnimationSetting setting = new SwipeAnimationSetting.Builder()
+                                        .setDirection(direction)
+                                        .setDuration(Duration.Normal.duration)
+                                        .build();
+
+        layoutManager.setSwipeAnimationSetting(setting);
+        binding.homeCardStackView.swipe();
     }
 }
